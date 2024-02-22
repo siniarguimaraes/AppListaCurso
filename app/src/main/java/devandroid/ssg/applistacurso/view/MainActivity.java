77ac003d86c46b9253c19d6820008c83fa2c5612
Sinair-Guimaraes <siniar_@hotmail.com>
2024-02-22 09:43:04 -0400

    Implementando Gets e Sets nos atributos dos Objetos.

--- app/src/main/java/devandroid/ssg/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/ssg/applistacurso/view/MainActivity.java
@@ -18,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Sinair");
+        pessoa.setSobreNome("Silva Guimarães");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("65-9614-6994");
+
 
     }
 }
