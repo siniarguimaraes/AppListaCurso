c472a55d0752d2f977820fc843f531dca817ec57
Sinair-Guimaraes <siniar_@hotmail.com>
2024-02-22 11:31:16 -0400

    Uso dos métodos Get e Set.

--- app/src/main/java/devandroid/ssg/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/ssg/applistacurso/view/MainActivity.java
@@ -11,6 +11,10 @@ import devandroid.ssg.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,26 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setSobreNome("Silva Guimarães");
         pessoa.setCursoDesejado("Android");
         pessoa.setTelefoneContato("65-9614-6994");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Luiz");
+        outraPessoa.setSobreNome("Alves");
+        outraPessoa.setCursoDesejado("Android");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("065 99664-3105");
+
+        dadosPessoa = "Primeiro nome : ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        int parada = 0;
+
+
 
 
     }
